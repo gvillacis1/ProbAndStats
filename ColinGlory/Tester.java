cb4ffb4ef77c033d2d3c5abd81b9a66a724140f4
gvillacis1 <114937814+gvillacis1@users.noreply.github.com>
2023-01-27 14:45:14 -0500

    This is a new push

---
+++ ColinGlory/Tester.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author galovillacis
+ *
+ *This class prints out Hello World to user.
+ *
+ */
+public class Tester {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+	}
+
+}
